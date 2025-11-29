e571111faf713b04680ffe780464e3c3125c97cc
Sipos Péter <sipos.peter@sysconet.hu>
2025-11-29 13:22:20 +0100

    Initial Commit

---
+++ DontDuckWithMyMac/DontDuckWithMyMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontDuckWithMyMacApp.swift
+//  DontDuckWithMyMac
+//
+//  Created by Péter Sipos on 2025. 11. 29..
+//
+
+import SwiftUI
+
+@main
+struct DontDuckWithMyMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
